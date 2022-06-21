ed3677bdb1bb6404808ca150de970562dc4c50be
info@coders.VN <CodersTokyo>
2022-06-22 01:25:35 +0700

    cấu hình try cập database file configuration

--- src/main/java/com/WebSaleOnline_1/Configuration/Configuration.java
+++ src/main/java/com/WebSaleOnline_1/Configuration/Configuration.java
@@ -1,7 +1,11 @@
 package com.WebSaleOnline_1.Configuration;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -23,5 +27,20 @@ public class Configuration implements WebMvcConfigurer{
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/Sell");
+		dataSource.setUsername("root");
+		dataSource.setPassword("24002948");
+		return dataSource;
+	}
+	
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+		return new JdbcTemplate(dataSource());
 	}
 }
